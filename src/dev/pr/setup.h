bc4cb43061d5aa0fa9bc84a20c211490fe573c8c
Sergey Ivanov <siv237@gmail.com>
2022-01-13 23:14:12 +1000

    правка для pr

--- src/dev/pr/setup.h
+++ src/dev/pr/setup.h
@@ -1,3 +1,6 @@
 #if c_PR == 1
-xTaskCreate(TaskPR,"TaskPR",10000,NULL,0,&appTasks[appTaskCount++]);
+pinMode(PR_AnalogPort, INPUT);
+adc1_config_width(ADC_WIDTH_BIT_12);
+adc1_config_channel_atten(PR_AnalogPort, ADC_ATTEN_DB_11);
+xTaskCreate(TaskPR, "TaskPR", 10000, NULL, 0, &appTasks[appTaskCount++]);
 #endif // c_PR
--- src/dev/pr/tasks.h
+++ src/dev/pr/tasks.h
@@ -2,25 +2,18 @@
 #if c_PR == 1
 void TaskPR(void *parameters)
 {
-  pinMode(PR_AnalogPort, INPUT);
-  
-  adc1_config_width(ADC_WIDTH_BIT_12);
-  adc1_config_channel_atten(PR_AnalogPort, ADC_ATTEN_DB_11);
-
   for (;;)
   {
     if (xSemaphore != NULL)
     {
       if (xSemaphoreTake(xSemaphore, (TickType_t)10) == pdTRUE)
       {
-    
-          PRRM.add ( adc1_get_raw(PR_AnalogPort));
-          PR = PRRM.getMedian();
-      
-          xSemaphoreGive(xSemaphore);
+        PRRM.add(adc1_get_raw(PR_AnalogPort));
+        PR = PRRM.getMedian();
+        xSemaphoreGive(xSemaphore);
       }
-    } 
-   vTaskDelay(5000 / portTICK_PERIOD_MS); 
+    }
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
   }
 }
 #endif //  c_PR
