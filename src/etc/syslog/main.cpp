8b8a65f8da19d9f61a65cb75694076c4985ae0fb
Sergey Ivanov <siv237@gmail.com>
2022-04-13 10:10:57 +1000

    Параметры syslog вынесены в файл pre.h

--- src/etc/syslog/main.cpp
+++ src/etc/syslog/main.cpp
@@ -1,8 +1,5 @@
 #if c_syslog == 1
     #include <Syslog.h>
-    #define SYSLOG_SERVER "192.168.237.111"
-    #define SYSLOG_PORT 514
-
     
     WiFiUDP udpClient;
     Syslog syslog(udpClient, SYSLOG_SERVER, SYSLOG_PORT, HOSTNAME, Firmware, LOG_KERN);
