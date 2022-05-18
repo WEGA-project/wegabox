15bf1b34166da5a6782ce4d63afb52725db922a1
Sergey Ivanov <siv237@gmail.com>
2022-05-18 13:27:45 +1000

    ...

--- src/dev/vl6180x/setup.h
+++ src/dev/vl6180x/setup.h
@@ -10,11 +10,11 @@
   // time to 30 ms and 50 ms, respectively, to allow 10 Hz
   // operation (as suggested by table "Interleaved mode
   // limits (10 Hz operation)" in the datasheet).
-  s_vl6180X.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
-  s_vl6180X.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+  //s_vl6180X.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
+  //s_vl6180X.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
   s_vl6180X.setScaling(3);
 
- // s_vl6180X.setTimeout(20);
+  //s_vl6180X.setTimeout(100);
 
    // stop continuous mode if already active
   s_vl6180X.stopContinuous();
@@ -22,7 +22,7 @@
   // measurement, wait for it to complete
   delay(300);
   // start interleaved continuous mode with period of 100 ms
- // s_vl6180X.startInterleavedContinuous(100);
+  //s_vl6180X.startInterleavedContinuous(100);
 
 
 xTaskCreate(TaskVL6180X,"VL6180X",10000,NULL,0,NULL);
