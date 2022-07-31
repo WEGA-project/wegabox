0bcd44b318b9a3c2d559ead328cec0732058eae7
Sergey Ivanov <siv237@gmail.com>
2022-07-31 13:34:53 +1000

    Вернул назад параетры опроса ЕС электрода, так как возможно текущие приводят к медленной деградации.

--- src/dev/ec/main.cpp
+++ src/dev/ec/main.cpp
@@ -3,10 +3,10 @@
   #define EC_DigitalPort1 18
   #define EC_DigitalPort2 19
   #define EC_AnalogPort ADC1_CHANNEL_5 // gpio33
-  #define EC_MiddleCount 50000  // 12000 в секунду
+  #define EC_MiddleCount 100000  // 12000 в секунду
 
 unsigned long EC_old = millis();
-unsigned long EC_Repeat=10000;
+unsigned long EC_Repeat=20000;
 
 RunningMedian ApRM = RunningMedian(10);
 RunningMedian AnRM = RunningMedian(10);
