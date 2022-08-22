60a24d08c7ae5b232445e6e19da9c40af3b49a85
Sergey Ivanov <siv237@gmail.com>
2022-08-22 23:25:49 +1000

    ...

--- src/dev/vl53l0x_us/tasks.h
+++ src/dev/vl53l0x_us/tasks.h
@@ -75,7 +75,7 @@ void TaskVL53L0X(void *parameters)
         //Dist = range / cont / 10;
 
          VL53L0X_RangeRM.add(range / (cont - err) /10);
-         Dist = VL53L0X_RangeRM.getMedian();
+         Dist = VL53L0X_RangeRM.getAverage(3);
 
         VL53L0X_time = millis() - VL53L0X_time;
 
