8240ad99c9c657552b9ba0ff6a4f21f8abf8b984
Sergey Ivanov <siv237@gmail.com>
2022-10-23 23:22:55 +1000

    ...

--- src/dev/ntc/setup.h
+++ src/dev/ntc/setup.h
@@ -2,6 +2,10 @@
 adc1_config_width(ADC_WIDTH_BIT_12);
 adc1_config_channel_atten(NTC_port, ADC_ATTEN_DB_11);
 
+if ( adc1_get_raw(NTC_port) > 0){
 xTaskCreate(TaskNTC,"TaskNTC",5000,NULL,0,NULL);
-syslog_ng("NTC add Task");
+syslog_ng("NTC: add Task");
+}
+else
+  syslog_err("NTC: The electrode is not detected");
 #endif
