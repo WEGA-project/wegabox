4353af297969c105e6db4b7e20268cd280f407a2
SIV237 <siv237@gmail.com>
2022-11-08 08:20:49 +1000

    ...

--- src/dev/doser/setup.h
+++ src/dev/doser/setup.h
@@ -3,6 +3,12 @@
 while (xSemaphoreTake(xSemaphoreX, (TickType_t)1) == pdFALSE)
   ;
 
+      mcp.pinMode(A, OUTPUT);     
+      mcp.pinMode(B, OUTPUT);     
+      mcp.pinMode(C, OUTPUT);     
+      mcp.pinMode(D, OUTPUT);  
+
+
   xTaskCreate(TaskDOSER, "TaskDOSER", 5000, NULL, 1, NULL);
   syslog_ng("DOSER: add Task");
 
