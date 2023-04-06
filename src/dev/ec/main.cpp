b96a809f8964dfb9a4a40c16da8d3d6fc2cebeed
Sergey Ivanov <siv237@gmail.com>
2023-04-06 04:11:59 +0000

    Работы по улучению стабильности ЕС и vl6180x

--- src/dev/ec/main.cpp
+++ src/dev/ec/main.cpp
@@ -1,17 +1,13 @@
 #if c_EC == 1
-#include <dev/old_adc/wega-adc.h>
-static _lock_t sar_adc1_lock;
-#define SAR_ADC1_LOCK_ACQUIRE() _lock_acquire(&sar_adc1_lock)
-#define SAR_ADC1_LOCK_RELEASE() _lock_release(&sar_adc1_lock)
 
   #define EC_DigitalPort1 18
   #define EC_DigitalPort2 19
-  #define EC_AnalogPort ADC1_CHANNEL_5 // gpio33
-  #define EC_MiddleCount 100000  // 12000 в секунду
+  #define EC_AnalogPort GPIO_NUM_33 // gpio33
+  #define EC_MiddleCount 60000 // 60000 за 1 сек
 
 unsigned long EC_old = millis();
-unsigned long EC_Repeat=20000;
+unsigned long EC_Repeat=5000;
 
-RunningMedian ApRM = RunningMedian(10);
-RunningMedian AnRM = RunningMedian(10);
+RunningMedian ApRM = RunningMedian(5);
+RunningMedian AnRM = RunningMedian(5);
 #endif
