407b82823188b7643fbce78eb4fdd49dd6ba81e7
Sergey Ivanov <siv237@gmail.com>
2023-04-06 06:37:58 +0000

    Опрос ес привязан к ядру для исключения изменения калибровки при ребутах

--- src/dev/ec/setup.h
+++ src/dev/ec/setup.h
@@ -6,7 +6,9 @@ if (adc1_get_raw(NTC_port) > 0)
 
    __wega_adcAttachPin(EC_AnalogPort);
    adc_power_acquire();
-   long EC_TaskErr = xTaskCreate(TaskEC, "TaskEC", 5000, NULL, 2, NULL);
+  // long EC_TaskErr = xTaskCreate(TaskEC, "TaskEC", 5000, NULL, 2, NULL);
+   long EC_TaskErr = xTaskCreatePinnedToCore (TaskEC, "TaskEC", 5000, NULL, 2, NULL, 1);
+
    //}
 
    syslog_ng("EC: add Task:" + fFTS(EC_TaskErr, 0));
