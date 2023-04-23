8d5509ec0dafed4d33ffdea2d5ea9be18ce606da
Sergey Ivanov <siv237@gmail.com>
2023-04-24 00:03:30 +1000

    тюнинг vl6180x

--- src/dev/vl6180x/setup.h
+++ src/dev/vl6180x/setup.h
@@ -8,6 +8,15 @@ if (Wire.available())
 {
 
 
+  s_vl6180X.init();
+  s_vl6180X.configureDefault();
+  s_vl6180X.setScaling(1);
+  s_vl6180X.setTimeout(100);
+  //s_vl6180X.stopContinuous();
+  s_vl6180X.startRangeContinuous();
+  s_vl6180X.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 20);
+  s_vl6180X.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
+
 
   xTaskCreate(TaskVL6180X, "VL6180X", 10000, NULL, 0, NULL);
   syslog_ng("VL6180X: add Task");
